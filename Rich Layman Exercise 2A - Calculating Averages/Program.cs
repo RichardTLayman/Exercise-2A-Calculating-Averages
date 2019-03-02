f4dee9ea540b905a92be259767a75558b9569e85
richardtlayman <richard.t.layman@gmail.com>
2019-03-01 19:09:53 -0500

    Add project files.

---
+++ Rich Layman Exercise 2A - Calculating Averages/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rich_Layman_Exercise_2A___Calculating_Averages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
